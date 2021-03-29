a3399d07cb806aa8bbc46159b8a06fafaaa0879d
roleqx <wizzsha@gmail.com>
2021-03-29 02:56:22 +0100

    Move the check for answer correctness at the room level

--- src/core/games/Shuffler.ts
+++ src/core/games/Shuffler.ts
@@ -26,41 +26,12 @@ export class Shuffler extends Room {
       resolve(this._shuffledWord);
     });
   }
-  protected onRoundStart(): void {
-    //
-  }
-
-  /**
-   * Check the answer of the player.
-   * @resolve Resolves to a number.
-   * - 0 = player input is acceptable but uncorrect (should be terated as a regular chat message).
-   * - 1 = player input is acceptable and is correct.
-   * - 2 = player has already find the answer and keeps sending more messages
-   * @reject The promise rejects when the user input in unacceptable/unexpected
-   */
-  public checkAnswer(player: Player, answer: string): Promise<number> {
-    return new Promise((resolve, reject) => {
-      // if the word to guess is not set yet
-      if (!this.wordToGuess) return reject;
-      // if the player has already found the answer and sends more messages while the round is still ongoing
-      if (player.hasAnswered) return resolve(2);
-      // if the word to guess lenght is not similar to the client answer
-      if (this.wordToGuess.length === answer.length) {
-        // if the answer is correct
-        if (this.wordToGuess == answer.toLocaleLowerCase()) {
-          // set the player as having found the correct anser
-          // TODO reset playerHasFoundAnswer on round end
-          resolve(1);
-        } else resolve(0); // incorrect answer/ regular chat message
-      } else {
-        // player answer is incorrect
-        resolve(0);
-      }
-    });
-    //
-  }
 
   shuffleWord(word: string): string {
     return arrayShuffle(word.split("")).join("");
+  }
+  // not needed in this game, but required by the parent class
+  onBeforeTurnStart(): string[] {
+    return [];
   }
 }
