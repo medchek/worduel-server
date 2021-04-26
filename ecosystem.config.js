8417a8533c1fb284b8992dfb53a0111e4c312a26
roleqx <wizzsha@gmail.com>
2021-04-26 07:15:18 +0100

    feat: reduce the number of cluster instances until implementing shared memory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,16 @@ module.exports = {
     {
       name: "worduel-server",
       script: "./build/index.js",
-      instances: "max",
+      watch: "./build/",
+      // Delay between restart
+      watch_delay: 1000,
+      ignore_watch: ["node_modules", "src"],
+      watch_options: {
+        followSymlinks: false,
+      },
+      instances: "1", // revert to max when implementing clustre-shared memory
+      exec_mode: "cluster",
+      instance_var: "INSTANCE_ID",
       env: {
         NODE_ENV: "development",
       },
